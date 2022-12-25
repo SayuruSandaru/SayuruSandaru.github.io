17089d27e0cab84886e48e849d8a40c7d87a905e
SayuruSandaru <sayurusadaru12@gmail.com>
2022-12-25 09:13:24 +0530

    updated nav bar

--- js/global.js
+++ js/global.js
@@ -1,6 +1,8 @@
 function onMenuClick() {
   var navbar = document.getElementById("navigation-bar");
   var responsive_class_name = "responsive";
-
+  var btn = document.getElementById("menu-icon");
+  var navheader = document.getElementById("nav-header");
   navbar.classList.toggle(responsive_class_name);
+  navheader.classList.toggle("active");
 }
